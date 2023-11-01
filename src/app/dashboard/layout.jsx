da8afc43cce62d18b7ba5dd40c3f962e4796ae33
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-11-01 12:45:18 +0530

    Creates Dashboard

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/DashboardSidebar/Sidebar";
+import React from "react";
+
+const Dashboardlayout = ({ children }) => {
+  return <section>
+    {children}</section>;
+};
+
+export default Dashboardlayout;
